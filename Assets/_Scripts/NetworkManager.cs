67aef6c3af4190d92c0f6d3c7ea24dad46663479
sdurlanik <sametdurlanik@gmail.com>
2022-04-07 17:49:13 +0300

    Network settings adjusted

---
+++ Assets/_Scripts/NetworkManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    //instance
+    public static NetworkManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        // master servera bağlanma
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+
+    // Yeni bir oda kurar veya odaya katılır
+    public void CreateOrJoinRoom()
+    {
+        // Katılabilecek bir oda varsa katılır
+        if (PhotonNetwork.CountOfRooms > 0)
+            PhotonNetwork.JoinRandomRoom();
+       // Katılabilecek oda yoksa yeni bir oda kurar
+        else
+        {
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = 2;
+
+            PhotonNetwork.CreateRoom(null, options);
+        }
+    }
+
+    // Sahne geçişinde kullanılır
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
